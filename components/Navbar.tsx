03479b1c88e58cf39a3b5fef45b6eee72a49f66c
obskut123 <tomasxd768@outlook.com>
2022-09-15 01:48:38 -0300

    avance un poco con el tema del diseno, estoy probando colores y layouts para la pagina, la idea sigue siendo simple

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,8 @@
-export function Navbar(props: any) {
+export function Navbar() {
     return (
-        <div class="flex w-full bg-black">
-            <h1 class="text-white">como va eso</h1>
+        <div class="flex w-full p-5 rounded shadow-xl" style={{backgroundColor: '#1C1C1C'}}>
+            <p class="text-3xl text-white font-bold">Skully Software</p>
+            <p class="text-3xl ml-auto animate-bounce">💀</p>
         </div>
     )
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,20 +1,43 @@
-import Counter from "../islands/Counter.tsx";
 import { Navbar } from "../components/Navbar.tsx";
 
 export default function Home() {
   return (
-    <div class="p-4 mx-auto max-w-screen-md">
-      <Navbar></Navbar>
-      <img
-        src="/logo.svg"
-        class="w-32 h-32"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-      />
-      <p class="my-6">
-        Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-        file, and refresh.
-      </p>
-      <Counter start={3} />
+    <div class="flex flex-col h-screen p-3" style={{backgroundColor: '#1C1C1C'}}>
+      <div class="flex flex-col p-4 mx-auto max-w-screen-md h-screen bg-gray-200 rounded shadow-2xl overflow-y-auto">
+        <Navbar></Navbar>
+        <div class="flex border-b-4 border-black my-6">
+          <h2 class="text-2xl text-black font-bold">Quien soy 👀?</h2>
+        </div>
+        <p class="text-black text-xl">
+          Soy Tomas Varas, desarrollador de software en mi ultimo año de analista programador,
+          actualmente de pasante en <a class="underline hover:text-blue-800" href="https://dilectosoftware.com.ar/" target="_blank">Dilecto Software</a> 🚀.  
+        </p> 
+        <div class="flex border-b-4 border-black my-6"> 
+        <h2 class="text-2xl text-black font-bold">Que es Skully Software 🦴?</h2> 
+        </div>  
+        <p class="text-black text-xl">
+          Skully software es mi proyecto para ofrecer servicios de desarrollo de software a corto y medio alcance,
+          enfocado en el desarrollo web y mobile.
+        </p> 
+        <div class="flex border-b-4 border-black my-6"> 
+          <h2 class="text-2xl text-black font-bold">Que tecnologias utilizo 💻?</h2> 
+        </div> 
+        <div class="grid grid-cols-4 gap-4 content-center place-items-center p-3 rounded shadow-xl" style={{backgroundColor: '#907AD6'}}>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg" class="h-16 w-16 hover:animate-spin"></img>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" class="h-16 w-16 hover:animate-spin"></img>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/e/e8/Deno_2021.svg" class="h-16 w-16 hover:animate-spin"></img>
+          <img src="https://www.svgrepo.com/show/331488/mongodb.svg" class="h-16 w-16 hover:animate-spin"></img>
+          <img src="https://cdn.worldvectorlogo.com/logos/firebase-1.svg" class="h-16 w-16 hover:animate-spin"></img>
+          <img src="https://cdn.worldvectorlogo.com/logos/typescript-2.svg" class="h-16 w-16 rounded hover:animate-spin"></img>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" class="h-16 w-16 rounded hover:animate-spin"></img>
+          <img src="https://seeklogo.com/images/P/python-logo-A32636CAA3-seeklogo.com.png" class="h-16 w-16 hover:animate-spin"></img>
+        </div>
+        <div class="mt-auto">
+          <a href="https://fresh.deno.dev">
+            <img width="197" height="37" src="https://fresh.deno.dev/fresh-badge-dark.svg" alt="Made with Fresh" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
