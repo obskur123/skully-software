a1a4d0f08bae5dcddf646e07712c0d31ef06e824
obskut123 <tomasxd768@outlook.com>
2022-09-16 00:10:14 -0300

    agregue una seccion de portafolio para mostrar una pequena intro a mis proyectos y empece una seccion de contacto al pie de la pag

--- routes/index.tsx
+++ routes/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         <p class="text-black text-xl">
           Skully software es mi proyecto para ofrecer servicios de desarrollo de software a corto y medio alcance,
           enfocado en el desarrollo web y mobile.
-        </p> 
+        </p>
         <div class="flex border-b-4 border-black my-6"> 
           <h2 class="text-2xl text-black font-bold">Que tecnologias utilizo 💻?</h2> 
         </div> 
@@ -32,7 +32,37 @@ export default function Home() {
           <img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" class="h-16 w-16 rounded hover:animate-spin"></img>
           <img src="https://seeklogo.com/images/P/python-logo-A32636CAA3-seeklogo.com.png" class="h-16 w-16 hover:animate-spin"></img>
         </div>
-        <div class="mt-auto">
+        <div class="flex border-b-4 border-black my-6"> 
+          <h2 class="text-2xl text-black font-bold">Portafolio</h2> 
+        </div>
+        <div class="grid grid-cols-1 gap-4 lg:grid-cols-2 mb-6 lg:mb-0">
+           <div class="flex flex-col rounded p-3 items-center shadow-xl hover:animate-pulse" style={{backgroundColor: '#d62826'}}> 
+              <div class="grid grid-cols-4 gap-3 place-items-center my-3">
+                <img src="https://avatars.githubusercontent.com/u/23064371?s=200&v=4" class="h-16 w-16" style={{borderRadius: '50%'}}></img> 
+                <img src="https://upload.wikimedia.org/wikipedia/commons/e/e8/Deno_2021.svg" class="h-16 w-16" style={{borderRadius: '50%'}}></img>
+                <img src="https://www.svgrepo.com/show/331488/mongodb.svg" class="h-16 w-16 hover:animate-spin"></img>
+                <img src="https://cdn.worldvectorlogo.com/logos/firebase-1.svg" class="h-16 w-16 hover:animate-spin"></img>
+              </div>
+              <h2 class="font-bold">Diccionario/Traductor Mapuzungun</h2>
+              <p class="text-center">Api y aplicacion para android para traducir palabras del español al mapuche de manera colectiva.</p>
+            </div>
+            <div class="flex flex-col rounded p-3 items-center shadow-xl hover:animate-pulse" style={{backgroundColor: '#2e72bc'}}>
+              <div class="grid grid-cols-2 gap-3 place-items-center my-3">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/e/e8/Deno_2021.svg" class="h-16 w-16 hover:animate-spin"></img>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" class="h-16 w-16 rounded hover:animate-spin"></img>
+              </div>
+              <h2 class="font-bold">Inmanga-scrapperapi</h2>
+              <p class="text-center">Api que scrapea la web inmanga.com y devuelve responses en forma de json para consumir
+              mangas en español en otras aplicaciones.</p>
+            </div>
+        </div>
+        <div class="flex border-b-4 border-black my-6">
+          <h2 class="text-2xl text-black font-bold">Contacto</h2>
+        </div>
+        <p class="text-black text-xl">
+          tomas_varas1@outlook.com
+        </p>
+        <div class="mt-auto flex justify-end">
           <a href="https://fresh.deno.dev">
             <img width="197" height="37" src="https://fresh.deno.dev/fresh-badge-dark.svg" alt="Made with Fresh" />
           </a>
